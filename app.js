fed396e62730a15ae643b670b1b87a9bbc6d3aae
Brian\Eric <limike126@gmail.com>
2020-05-21 09:27:18 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+var createError = require('http-errors');
+var express = require('express')
+  , engine = require('ejs-locals')
+  , app = express();
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var cors = require('cors');
+var mongoose = require('mongoose');
+var session = require('express-session')
+var passport = require('passport')
+var flash = require('connect-flash')
+var validator = require('express-validator')
+
+var app = express();
+
+const MONGOURL = process.env.MONGODB_URI || 'mongodb://localhost/test';
+mongoose.connect(MONGOURL, { useNewUrlParser: true }, err => {
+  console.error(err || `Connected to MongoDB: ${MONGOURL}`);
+});
+require('./config/passport')
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('ejs', engine);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(validator());
+app.use(cookieParser());
+app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized:false}))
+app.use(flash());
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+  res.locals.login = req.isAuthenticated();
+  next();
+})
+
+app.use('/', indexRouter);
+app.use('/user', usersRouter);
+app.use(cors());
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var router = express.Router();
+var mainController = require('../controllers/main.controller')
+var main = new mainController();
+var bodyParser = require('body-parser');
+var User = require('../models/user');
+var Booking = require('../models/booking')
+router.use(bodyParser.json());
+
+var csrf = require('csurf');
+var csrfProtection = csrf();
+
+var passport = require('passport')
+
+router.use(csrfProtection)
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // console.log("----------user----------",req.user)
+  res.render('index', { user: req.user });
+});
+
+router.get('/about', function(req, res, next) {
+  // console.log("----------user----------",req.user)
+  res.render('about', { user: req.user });
+});
+
+router.get('/products', function(req, res, next) {
+  // console.log("----------user----------",req.user)
+  res.render('products', { user: req.user });
+});
+
+router.get('/book', isLoggedIn, function(req, res, next){
+  res.render('booking', { csrfToken: req.csrfToken(), user: req.user })
+})
+
+router.get('/dashboard', isLoggedIn, function(req, res, next){
+  if(req.user.role == 2){
+    res.redirect('/dashboard/restaurant');
+  }
+  else if(req.user.role == 3){
+    res.redirect('/dashboard/user');
+  }
+  
+})
+
+router.get('/dashboard/restaurant', isLoggedIn, function(req, res, next){
+  if(req.user.role == 2 || req.user.role == 3){
+    Booking.find(function(err, docs){
+      res.render('dashboard/res_manage', { csrfToken: req.csrfToken(), user: req.user, bookdata: docs})
+    })
+  }
+  else{
+    res.redirect('/');
+  }
+  
+})
+
+router.get('/dashboard/user', isLoggedIn, function(req, res, next){
+  if(req.user.role == 3){
+    User.find(function(err, docs) {
+      res.render('dashboard/user_manage', { csrfToken: req.csrfToken(), user: req.user, userdata: docs})
+    })
+  }
+  else{
+    res.redirect('/');
+  }
+})
+
+
+router.get('/userDelete/:userID', isLoggedIn, main.deleteUser)
+router.post('/userUpdate', isLoggedIn, main.updateUser)
+
+router.post('/booking', isLoggedIn, main.addBooking)
+router.get('/approveBooking/:userID', isLoggedIn, main.approveBooking)
+router.get('/rejectBooking/:userID', isLoggedIn, main.rejectBooking)
+
+router.get('/test', main.test);
+
+module.exports = router;
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/');
+}
